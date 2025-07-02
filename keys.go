405c8c4dbceb52ffa4f09b101d9e5b3973652328
MBSA <marinacompsci@protonmail.com>
2025-07-02 20:32:00 +0200

    refactor: rewrite comments

--- keys.go
+++ keys.go
@@ -2,15 +2,15 @@ package main
 
 type Key int 
 
-// Special keys
+// Non-printable keys
 const (
 	KEY_CTRL_C Key		= 3
 	KEY_ESC Key			= 27
 	KEY_DEL Key 		= 127
 )
 
+// Navigation keys
 const (
-	//TODO: first I must enter into selection mode with ESC
 	KEY_DOWN Key 		= 106 // 'J' and not ARROW KEY DOWN
 	KEY_UP Key 			= 107 // 'K' and not ARROW KEY UP
 )
